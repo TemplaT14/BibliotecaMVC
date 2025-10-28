54e431520023638605fae64fc7f746fc73fae47a
TemplaT14 <jose.martiingles@gmail.com>
2025-10-28 09:14:09 +0100

    Terminado

--- src/main/java/es/dam/accesodatos/dao/Conexion.java
+++ src/main/java/es/dam/accesodatos/dao/Conexion.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL  = "jdbc:mysql://localhost:3307/biblioteca?serverTimezone=UTC";
-    private static final String USER = "root";
+    private static final String URL  = "jdbc:mysql://localhost:3306/biblioteca?serverTimezone=UTC";
+    private static final String USER = "jovaR";
     private static final String PASS = "123456";
 
     public static Connection get() throws SQLException {
